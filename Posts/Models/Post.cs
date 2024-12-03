d0656f9f0b6e539e4cf6f47e04a7d66a9bfbae92
Spencer Rauch <rauchs@gmail.com>
2024-12-03 09:49:26 -0800

    algos and morning lecture

---
+++ Posts/Models/Post.cs
@@ -0,0 +1,29 @@
+#pragma warning disable CS8618
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Posts.Models;
+
+public class Post
+{
+    [Key]
+    public int PostId { get;set; }
+
+    [Required]
+    [MinLength(2)]
+    [MaxLength(30)]
+    public string Title { get;set; }
+
+    [Required]
+    [MinLength(2)]
+    public string Body { get;set; }
+
+    [DisplayName("Image URL")]
+    public string? ImgURL { get;set; }
+
+    public DateTime CreatedAt {get;set;} = DateTime.Now;
+    public DateTime UpdatedAt {get;set;} = DateTime.Now;   
+
+
+}
