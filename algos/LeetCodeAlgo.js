23889c5427ad6fab8c7dc1fb17e0f86f6f0ee808
Spencer Rauch <rauchs@gmail.com>
2024-12-19 07:56:23 -0800

    algo

---
+++ algos/LeetCodeAlgo.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ *   https://leetcode.com/problems/intersection-of-two-linked-lists/
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    //your code here
+};
