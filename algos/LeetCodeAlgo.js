80d5788838a9b46b534461b4becde3d3b3858752
Spencer Rauch <rauchs@gmail.com>
2024-12-19 08:27:24 -0800

    solutions

--- algos/LeetCodeAlgo.js
+++ algos/LeetCodeAlgo.js
@@ -13,5 +13,83 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    //your code here
+    if(headA===headB) return headA;
+    let runnerA = headA
+    let runnerB = headB
+    let countA = 0
+    let countB = 0
+
+    //lists that intersect will have the same tail
+    //traverse to tail, calculate length on the way
+    while(runnerA.next || runnerB.next)
+    {
+        if (runnerA.next){
+            countA++;
+            runnerA = runnerA.next;
+        } 
+        if (runnerB.next){
+            countB++;
+            runnerB = runnerB.next;
+        } 
+    }
+    //no intersection
+    if (runnerA !== runnerB) return null;
+
+    //reset runners
+    runnerA = headA;
+    runnerB = headB;
+
+    // if one list is longer, move forward in that list
+    // so that lists are equally away from intersection
+    if(countA !== countB)
+    {
+        while (countA > countB)
+        {
+            runnerA = runnerA.next;
+            countA--;
+        }
+        while (countB > countA)
+        {
+            runnerB = runnerB.next;
+            countB--;
+        }
+    }
+
+    //move heads forward to look for intersection
+    while (runnerA !== runnerB)
+    {
+        runnerA = runnerA.next;
+        runnerB = runnerB.next;
+    }
+
+    return runnerA
+
+};
+
+
+
+
+var getIntersectionNode = function(headA, headB) {
+    const AMap = new Map();
+    while(headA)
+    {
+        AMap.set(headA, 0);
+        headA = headA.next;
+    }
+
+    while(headB)
+    {
+        if (AMap.has(headB)) return headB;
+        headB = headB.next
+    }
+    return null
+};
+
+var getIntersectionNode = function(headA, headB) {
+    let a = headA, b = headB
+    while (a !== b) {
+        a = !a ? headB : a.next
+        b = !b ? headA : b.next
+    }
+    return a
 };
